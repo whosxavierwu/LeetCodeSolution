b5801f3042ea258f3fe9f028547ce667dec6cbb0
xavier wu <xaverwu@outlook.com>
2019-07-04 16:38:48 +0800

    169 & 206 completed

---
+++ leetcode_206/leetcode_206.cpp
@@ -0,0 +1,41 @@
+// leetcode_206.cpp : This file contains the 'main' function. Program execution begins and ends there.
+// https://leetcode.com/problems/reverse-linked-list/
+// A linked list can be reversed either iteratively or recursively. Could you implement both?
+
+#include <iostream>
+
+
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode(int x) : val(x), next(NULL) {}
+};
+
+// todo how about recursive version?
+class Solution {
+public:
+	ListNode* reverseList(ListNode* head) {
+		ListNode* new_head = NULL;
+		ListNode* tmp = NULL;
+		while (head) {
+			if (new_head == NULL) {
+				new_head = head;
+				head = head->next;
+				new_head->next = NULL;
+			}
+			else {
+				tmp = head->next;
+				head->next = new_head;
+				new_head = head;
+				head = tmp;
+			}
+		}
+		return new_head;
+	}
+};
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
+
