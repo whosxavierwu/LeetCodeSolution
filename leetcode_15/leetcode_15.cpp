32e8e9439bec09f02d5dc292edae8b7c3d3652c0
wuzewei <wuzewei@autohome.com.cn>
2019-07-14 11:31:03 +0800

    completed 15

--- leetcode_15/leetcode_15.cpp
+++ leetcode_15/leetcode_15.cpp
@@ -1,51 +1,107 @@
 // leetcode_15.cpp : This file contains the 'main' function. Program execution begins and ends there.
-// 3Sum, find out triplets that sum to zero.
+// https://leetcode.com/problems/3sum/
 
 #include <iostream>
 #include <vector>
+#include <algorithm>
+#include <set>
 using namespace std;
 
 class Solution {
 public:
-	int binarySearch(vector<int>& nums, int start, int end, int target) {
-		int mid = start;
-		while (start <= end) {
-			mid = (start + end) / 2;
-			// to avoid overflow: 
-			//mid = start + (end - start) / 2;
-			if (nums[mid] == target)
-				return mid;
-			else if (nums[mid] < target)
-				start = mid + 1;
-			else
-				end = mid - 1;
-		}
-		return -1;
-	}
-
-	vector<vector<int>> twoSum(vector<int>& numbers, int start, int end, int target) {
-		vector<vector<int>> result(0);
-		int tmp = 0;
-		for (int i = start; i <= (end-2); ++i) {
-			tmp = binarySearch(numbers, i + 1, end, target - numbers[i]);
-			if (tmp >= 0) 
-				result.push_back({numbers[i], numbers[tmp]});
-		}
-		return result;
-	}
-
 	vector<vector<int>> threeSum(vector<int>& nums) {
-		// v1: refer to twoSum from #167
-		// we have problem! The solution set must not contain duplicate triplets
 		int len = nums.size();
 		vector<vector<int>> results(0);
-		for (int i = 0; i <= (len-3); ++i) {
-			vector<vector<int>> twins = twoSum(nums, i + 1, len - 1, -nums[i]);
-			for (vector<int> twin : twins) {
-				results.push_back({ nums[i], twin[0], twin[1] });
+		if (len <= 3) {
+			if ((len == 3) && (nums[0] + nums[1] + nums[2] == 0)) 
+				results.push_back(nums);
+			return results;
+		}
+
+		// v2: faster than 38.97%
+		//sort(nums.begin(), nums.end());
+		//for (int i = 0; i < len; ++i) {
+		//	if (i != 0 && nums[i - 1] == nums[i])
+		//		// help to drop duplicates?
+		//		continue;
+		//	int j = i + 1, k = len - 1;
+		//	while (j < k) {
+		//		if (nums[i] + nums[j] + nums[k] > 0)
+		//			--k;
+		//		else if (nums[i] + nums[j] + nums[k] < 0)
+		//			++j;
+		//		else {
+		//			results.push_back({ nums[i], nums[j], nums[k] });
+		//			// help to drop dupllicates?
+		//			while (j < k && nums[j + 1] == nums[j]) ++j;
+		//			while (j < k && nums[k - 1] == nums[k]) --k;
+		//			++j;
+		//			--k;
+		//		}
+		//	}
+		//}
+		//return results;
+
+		// v3: faster than 95.13%
+		//sort(nums.begin(), nums.end());
+		//for (int i = 0; i < len; ++i) {
+		//	int target = -nums[i];
+		//	int front = i + 1;
+		//	int back = len - 1;
+
+		//	// front>i & back>i, so both of them less than target.
+		//	// if target<0, front<0 and back<0, so sum could never be zero
+		//	if (target < 0)
+		//		break;  
+
+		//	while (front < back) {
+		//		int sum = nums[front] + nums[back];
+		//		// nums[front]<nums[back]
+		//		if (sum < target)
+		//			// we need to +front; since we cant +back
+		//			++front;  
+		//		else if (sum > target)
+		//			// we need to -back; since we cant -front
+		//			--back;
+		//		else {
+		//			vector<int> triplet = { nums[i], nums[front], nums[back] };
+		//			results.push_back(triplet);
+
+		//			// drop duplicates
+		//			while (front < back && nums[front] == triplet[1]) ++front;
+		//			while (front < back && nums[back] == triplet[2]) --back;
+		//		}
+		//	}
+		//	// speed up 
+		//	while (i + 1 < len && nums[i + 1] == nums[i]) ++i;
+		//}
+		//return results;
+
+		// v4: faster than 95.13%
+		sort(nums.begin(), nums.end());
+		for (int i = 0; i < len - 2; ++i) {
+			int a = nums[i];
+			if (a > 0) break;
+			if (i > 0 && a == nums[i - 1]) continue;
+			for (long j = i + 1, k = len - 1; j < k;) {
+				int b = nums[j];
+				int c = nums[k];
+				int sum = a + b + c;
+				if (sum == 0) {
+					results.push_back(vector<int>({ a, b, c }));
+					while (j < k && b == nums[++j]);
+					while (j < k && c == nums[--k]);
+				}
+				else if (sum > 0) {
+					--k;
+				}
+				else {
+					++j;
+				}
 			}
 		}
 		return results;
+
 	}
 };
 
