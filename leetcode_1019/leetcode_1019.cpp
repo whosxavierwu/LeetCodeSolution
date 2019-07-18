fb551179298ac6dd025cd3b0bc96d66ef20d0782
xavier wu <xaverwu@outlook.com>
2019-07-18 20:57:19 +0800

    update

--- leetcode_1019/leetcode_1019.cpp
+++ leetcode_1019/leetcode_1019.cpp
@@ -38,29 +38,82 @@ class Solution {
 public:
 	vector<int> nextLargerNodes(ListNode* head) {
 		// v1: faster than 16.15% , too slow!!!
+		//vector<int> result;
+		//for (ListNode* p = head; p != NULL; p = p->next) {
+		//	if (p->next == NULL) {
+		//		result.push_back(0);
+		//		break;
+		//	}
+		//	bool found = false;
+		//	for (ListNode* q = p->next; q != NULL; q = q->next) {
+		//		if (q->val > p->val) {
+		//			result.push_back(q->val);
+		//			found = true;
+		//			break;
+		//		}
+		//	}
+		//	if (!found)
+		//		result.push_back(0);
+		//}
+		//return result;
+
+		// v2: WA
 		vector<int> result;
-		for (ListNode* p = head; p != NULL; p = p->next) {
+		ListNode* p = head;
+		while (p != NULL) {
 			if (p->next == NULL) {
 				result.push_back(0);
 				break;
 			}
-			bool found = false;
-			for (ListNode* q = p->next; q != NULL; q = q->next) {
-				if (q->val > p->val) {
-					result.push_back(q->val);
-					found = true;
+			int cnt = 1;
+			ListNode* q = p->next;
+			while (q != NULL) {
+				if (q->val < p->val) {
+					++cnt;
+				}
+				else if (q->val == p->val) {
+					// todo error here
+					++cnt;
+				}
+				else {
+					for (int i = 0; i < cnt; ++i)
+						result.push_back(q->val);
+					p = q;
 					break;
 				}
+				q = q->next;
 			}
-			if (!found)
+			if (q == NULL) {
 				result.push_back(0);
+				p = p->next;
+			}
 		}
+		
 		return result;
 	}
 };
 
+ListNode* arr2llist(vector<int>& arr) {
+	int len = size(arr);
+	ListNode* tmp_node = new ListNode(arr[0]);
+	ListNode* llist = tmp_node;
+	ListNode* cur = llist;
+	for (int i = 1; i < len; i++) {
+		tmp_node = new ListNode(arr[i]);
+		cur->next = tmp_node;
+		cur = cur->next;
+	}
+	return llist;
+}
+
 int main()
 {
-    std::cout << "Hello World!\n";
+	Solution sol;
+	//vector<int> arr = { 2,7,4,3,5 };
+	vector<int> arr = { 9,7,6,7,6,9 };
+	ListNode* head = arr2llist(arr);
+	vector<int> result = sol.nextLargerNodes(head);
+	for (int i = 0; i < result.size(); ++i)
+		cout << result[i] << endl;
 }
 
