dd1e886c42db28a1dade6c94d8cb6002aae18c27
xavier wu <xaverwu@outlook.com>
2019-07-18 11:17:57 +0800

    completed #2

--- leetcode_2/leetcode_2.cpp
+++ leetcode_2/leetcode_2.cpp
@@ -1,20 +1,64 @@
 // leetcode_2.cpp : This file contains the 'main' function. Program execution begins and ends there.
 // https://leetcode.com/problems/add-two-numbers/
+/*
+You are given two non-empty linked lists representing two non-negative integers. 
+The digits are stored in reverse order and each of their nodes contain a single digit. 
+Add the two numbers and return it as a linked list.
 
+You may assume the two numbers do not contain any leading zero, except the number 0 itself.
+
+Input: (2 -> 4 -> 3) + (5 -> 6 -> 4)
+Output: 7 -> 0 -> 8
+342 + 465 = 807.
+*/
 #include <iostream>
+
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode(int x) : val(x), next(NULL) {}
+};
+
+class Solution {
+public:
+	ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
+		// v1: faster than 74.41%
+		ListNode* head = NULL;
+		ListNode* tail = NULL;
+		ListNode* p1 = l1;
+		ListNode* p2 = l2;
+		int carry = 0;
+		while (p1 != NULL || p2 != NULL) {
+			int val = carry;
+			if (p1 != NULL) {
+				val += p1->val;
+				p1 = p1->next;
+			}
+			if (p2 != NULL) {
+				val += p2->val;
+				p2 = p2->next;
+			}
+			carry = val / 10;
+			val = val % 10;
+			ListNode* tmp = new ListNode(val);
+			if (head == NULL) {
+				head = tmp;
+				tail = tmp;
+			}
+			else {
+				tail->next = tmp;
+				tail = tail->next;
+			}
+		}
+		if (carry > 0) {
+			tail->next = new ListNode(carry);
+			tail = tail->next;
+		}
+		return head;
+	}
+};
 
 int main()
 {
     std::cout << "Hello World!\n";
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
--- leetcode_25/leetcode_25.cpp
+++ leetcode_25/leetcode_25.cpp
@@ -1,20 +1,40 @@
 // leetcode_25.cpp : This file contains the 'main' function. Program execution begins and ends there.
 // https://leetcode.com/problems/reverse-nodes-in-k-group/
+/*
+Given a linked list, reverse the nodes of a linked list k at a time and return its modified list.
+
+k is a positive integer and is less than or equal to the length of the linked list. 
+If the number of nodes is not a multiple of k then left-out nodes in the end should remain as it is.
+
+Example:
+Given this linked list: 1->2->3->4->5
+
+For k = 2, you should return: 2->1->4->3->5
+
+For k = 3, you should return: 3->2->1->4->5
+
+Note:
+
+Only constant extra memory is allowed.
+You may not alter the values in the list's nodes, only nodes itself may be changed.
+*/
 
 #include <iostream>
+
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode(int x) : val(x), next(NULL) {}
+};
+
+class Solution {
+public:
+	ListNode* reverseKGroup(ListNode* head, int k) {
+
+	}
+};
 
 int main()
 {
     std::cout << "Hello World!\n";
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
