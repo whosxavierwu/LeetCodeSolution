fac6bfc26b4c6d028eccc6ad250135deb6581d01
xavier wu <xaverwu@outlook.com>
2019-07-18 18:22:32 +0800

    update

--- leetcode_1019/leetcode_1019.cpp
+++ leetcode_1019/leetcode_1019.cpp
@@ -1,20 +1,66 @@
 // leetcode_1019.cpp : This file contains the 'main' function. Program execution begins and ends there.
 // https://leetcode.com/problems/next-greater-node-in-linked-list/
+/*
+We are given a linked list with head as the first node.  
+Let's number the nodes in the list: node_1, node_2, node_3, ... etc.
+Each node may have a next larger value: for node_i, next_larger(node_i) is 
+the node_j.val such that j > i, node_j.val > node_i.val, and j is the smallest possible choice.  
+
+If such a j does not exist, the next larger value is 0.
+Return an array of integers answer, where answer[i] = next_larger(node_{i+1}).
+
+Input: [2,1,5]
+Output: [5,5,0]
+
+Input: [2,7,4,3,5]
+Output: [7,0,5,5,0]
+
+Input: [1,7,5,1,9,2,5,1]
+Output: [7,9,9,9,0,5,0,0]
+
+Note:
+
+1 <= node.val <= 10^9 for each node in the linked list.
+The given list has length in the range [0, 10000].
+*/
 
 #include <iostream>
+#include <vector>
+using namespace std;
+
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode(int x) : val(x), next(NULL) {}
+};
+
+class Solution {
+public:
+	vector<int> nextLargerNodes(ListNode* head) {
+		// v1: faster than 16.15% , too slow!!!
+		vector<int> result;
+		for (ListNode* p = head; p != NULL; p = p->next) {
+			if (p->next == NULL) {
+				result.push_back(0);
+				break;
+			}
+			bool found = false;
+			for (ListNode* q = p->next; q != NULL; q = q->next) {
+				if (q->val > p->val) {
+					result.push_back(q->val);
+					found = true;
+					break;
+				}
+			}
+			if (!found)
+				result.push_back(0);
+		}
+		return result;
+	}
+};
 
 int main()
 {
     std::cout << "Hello World!\n";
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
--- leetcode_92/leetcode_92.cpp
+++ leetcode_92/leetcode_92.cpp
@@ -1,20 +1,31 @@
 // leetcode_92.cpp : This file contains the 'main' function. Program execution begins and ends there.
 // https://leetcode.com/problems/reverse-linked-list-ii/
+/*
+Reverse a linked list from position m to n. Do it in one-pass.
+Note: 1 ¡Ü m ¡Ü n ¡Ü length of list.
 
+Example:
+
+Input: 1->2->3->4->5->NULL, m = 2, n = 4
+Output: 1->4->3->2->5->NULL
+*/
 #include <iostream>
 
+
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode(int x) : val(x), next(NULL) {}
+};
+
+class Solution {
+public:
+	ListNode* reverseBetween(ListNode* head, int m, int n) {
+
+	}
+};
 int main()
 {
     std::cout << "Hello World!\n";
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
