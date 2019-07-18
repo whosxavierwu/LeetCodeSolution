67cacd0061336c52b75c3c0850de472023111147
xavier wu <xaverwu@outlook.com>
2019-07-18 14:10:57 +0800

    completed #25

--- leetcode_25/leetcode_25.cpp
+++ leetcode_25/leetcode_25.cpp
@@ -20,6 +20,8 @@ You may not alter the values in the list's nodes, only nodes itself may be chang
 */
 
 #include <iostream>
+#include <vector>
+using namespace std;
 
 struct ListNode {
     int val;
@@ -29,12 +31,75 @@ struct ListNode {
 
 class Solution {
 public:
+	ListNode* reverseSingleGroup(ListNode* oldHead, ListNode* oldTail) {
+		// IN: oldHead -> a1->...ak -> oldTail
+		// OUT: oldTail->ak->...a1 ->oldHead
+		ListNode* newHead = oldTail->next;
+		ListNode* newTail = oldTail->next;
+		ListNode* cur = oldHead;
+		while (cur != newTail) {
+			ListNode *next = cur->next;
+			cur->next = newHead;
+			newHead = cur;
+			cur = next;
+		}
+		return newHead;
+	}
 	ListNode* reverseKGroup(ListNode* head, int k) {
-
+		// v1: faster than 97.84% 
+		if (k <= 1)
+			return head;
+		ListNode* newHead = NULL;
+		ListNode* newTail = NULL;
+		ListNode* tmpHead = head;
+		ListNode* tmpTail = head;
+		bool isEnd = false;
+		while (!isEnd) {
+			for (int i = 0; i < k-1; ++i) {
+				if (tmpTail == NULL) {
+					isEnd = true;
+					break;
+				}
+				tmpTail = tmpTail->next;
+			}
+			if (isEnd || tmpTail == NULL) {
+				break;
+			}
+			if (newHead == NULL) {
+				newHead = reverseSingleGroup(tmpHead, tmpTail);
+			}
+			else {
+				newTail->next = reverseSingleGroup(tmpHead, tmpTail);
+			}
+			newTail = tmpHead;
+			tmpHead = newTail->next;
+			tmpTail = tmpHead;
+		}
+		if (newHead == NULL)
+			newHead = head;
+		return newHead;
 	}
 };
 
+ListNode* arr2llist(vector<int>& arr) {
+	int len = size(arr);
+	ListNode* tmp_node = new ListNode(arr[0]);
+	ListNode* llist = tmp_node;
+	ListNode* cur = llist;
+	for (int i = 1; i < len; i++) {
+		tmp_node = new ListNode(arr[i]);
+		cur->next = tmp_node;
+		cur = cur->next;
+	}
+	return llist;
+}
+
 int main()
 {
-    std::cout << "Hello World!\n";
+	Solution sol; 
+	vector<int> arr = { 1, 2, 3, 4, 5 };
+	int k = 2;
+	ListNode* head = arr2llist(arr);
+	ListNode* result = sol.reverseKGroup(head, k);
+	return 0;
 }
