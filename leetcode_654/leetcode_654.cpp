ed769d1027e95f345f660cbe2c1638d9947d4913
xavier wu <xaverwu@outlook.com>
2019-07-22 12:50:03 +0800

    completed 654

--- leetcode_654/leetcode_654.cpp
+++ leetcode_654/leetcode_654.cpp
@@ -10,6 +10,9 @@ Construct the maximum tree by the given array and output the root node of this t
 */
 #include <iostream>
 #include <vector>
+#include <stack>
+#include <algorithm>
+#include <map>
 using namespace std;
 
 struct TreeNode {
@@ -21,12 +24,79 @@ struct TreeNode {
 
 class Solution {
 public:
+	TreeNode* constructFunc(vector<int>& nums, int start, int end) {
+		int maxIdx = start;
+		for (int i = start + 1; i <= end; ++i) {
+			if (nums[i] > nums[maxIdx])
+				maxIdx = i;
+		}
+		TreeNode* root = new TreeNode(nums[maxIdx]);
+		if (maxIdx - 1 > start)
+			root->left = constructFunc(nums, start, maxIdx - 1);
+		else if (maxIdx - 1 == start)
+			root->left = new TreeNode(nums[start]);
+		if (end > maxIdx + 1)
+			root->right = constructFunc(nums, maxIdx + 1, end);
+		else if (end == maxIdx + 1)
+			root->right = new TreeNode(nums[end]);
+		return root;
+	}
 	TreeNode* constructMaximumBinaryTree(vector<int>& nums) {
+		// v1: recursive, faster than 83.17% 
+		//return constructFunc(nums, 0, nums.size() - 1);
+
+		//// v2: one pass  faster than 83.17% why so slow???
+		//// https://leetcode.com/problems/maximum-binary-tree/discuss/106146/C%2B%2B-O(N)-solution
+		//vector<TreeNode*> stk;
+		//for (int i = 0; i < nums.size(); ++i) {
+		//	TreeNode* cur = new TreeNode(nums[i]);
+		//	// top of stack is always >= nums[i]
+		//	while (!stk.empty() && stk.back()->val < nums[i]) {
+		//		// refreshing to make cur->left the largest num 
+		//		// that is before nums[i]
+		//		cur->left = stk.back();
+		//		stk.pop_back();
+		//	}
+		//	// top of stack >= cur
+		//	// and cur is on the right of stack.top
+		//	if (!stk.empty())
+		//		stk.back()->right = cur;
+		//	stk.push_back(cur);
+		//}
+		//// the greatest num
+		//return stk.front();
+
+		//// v3: faster than 68.71% 
+		//map<int, TreeNode*> q;
+		//for (int num : nums) {
+		//	auto it = q.insert({ num, new TreeNode(num) }).first;
+		//	if (it != q.begin()) {
+		//		it->second->left = next(it, -1)->second;
+		//		q.erase(q.begin(), it);
+		//	}
+		//	if (next(it, 1) != q.end()) next(it, 1)->second->right = it->second;
+		//}
+		//return q.rbegin()->second;
+
+		// v4 faster than 83.17% ...
+		vector<TreeNode*> s{ new TreeNode(nums[0]) };
+		for (int i = 1; i < nums.size(); ++i) {
+			TreeNode* cur = new TreeNode(nums[i]);
+			auto it = upper_bound(s.rbegin(), s.rend(), cur,
+				[](const TreeNode* a, const TreeNode* b) { return a->val < b->val; });
+			if (it != s.rend()) (*it)->right = cur;
+			if (it != s.rbegin()) cur->left = *next(it, -1);
+			s.resize(distance(it, s.rend()));
+			s.push_back(cur);
+		}
+		return s.front();
 
 	}
 };
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	Solution sol;
+	vector<int> nums = { 3,2,1,6,0,5 };
+	TreeNode* root = sol.constructMaximumBinaryTree(nums);
 }
