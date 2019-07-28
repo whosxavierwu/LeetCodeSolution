b56ada772f4851c3434a0930c28cbd31cebd6379
wuzewei <wuzewei@autohome.com.cn>
2019-07-28 17:09:10 +0800

    init 347

---
+++ leetcode_347/leetcode_347.cpp
@@ -0,0 +1,18 @@
+// leetcode_347.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+	vector<int> topKFrequent(vector<int>& nums, int k) {
+
+	}
+};
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
