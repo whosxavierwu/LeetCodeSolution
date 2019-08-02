4c267d039227daafed603135170cdab9c2fac57b
xavier wu <xaverwu@outlook.com>
2019-08-02 12:07:50 +0800

    completed 215

--- leetcode_215/leetcode_215.cpp
+++ leetcode_215/leetcode_215.cpp
@@ -3,12 +3,24 @@
 
 #include <iostream>
 #include <vector>
+#include <algorithm>
+#include <queue>
 using namespace std;
 
 class Solution {
 public:
 	int findKthLargest(vector<int>& nums, int k) {
+		// v1 sort faster than 80.23% 
+		//sort(nums.begin(), nums.end());
+		//return nums[nums.size() - k];
 
+		// v2 faster than 97.71% 
+		priority_queue<int> pq;
+		for (int num : nums)
+			pq.push(num);
+		for (int i = 1; i < k; ++i)
+			pq.pop();
+		return pq.top();
 	}
 };
 
