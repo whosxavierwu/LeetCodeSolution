ef939a90705a3cc1dcdbd80f93d85cb99f65e175
xavier wu <xaverwu@outlook.com>
2019-08-02 11:56:23 +0800

    completed 102

--- leetcode_102/leetcode_102.cpp
+++ leetcode_102/leetcode_102.cpp
@@ -3,6 +3,7 @@
 
 #include <iostream>
 #include <vector>
+#include <deque>
 using namespace std;
 
 struct TreeNode {
@@ -15,7 +16,25 @@ struct TreeNode {
 class Solution {
 public:
 	vector<vector<int>> levelOrder(TreeNode* root) {
-
+		// v1: faster than 93.84% 
+		if (!root) return {};
+		deque<TreeNode*> deq;
+		deq.push_back(root);
+		vector<vector<int>> result;
+		while (!deq.empty()) {
+			vector<int> s;
+			int len = deq.size();
+			for (int i = 0; i < len; ++i) {
+				s.push_back(deq.front()->val);
+				if (deq.front()->left)
+					deq.push_back(deq.front()->left);
+				if (deq.front()->right)
+					deq.push_back(deq.front()->right);
+				deq.pop_front();
+			}
+			result.push_back(s);
+		}
+		return result;
 	}
 };
 int main()
