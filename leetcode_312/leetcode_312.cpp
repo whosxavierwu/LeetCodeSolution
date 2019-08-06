049772fa2edf33196b9ec6f034423a84e170822f
xavier wu <xaverwu@outlook.com>
2019-08-06 09:40:28 +0800

    init projects

---
+++ leetcode_312/leetcode_312.cpp
@@ -0,0 +1,26 @@
+// leetcode_312.cpp : This file contains the 'main' function. Program execution begins and ends there.
+// https://leetcode.com/problems/burst-balloons/
+/*
+Given n balloons, indexed from 0 to n-1. Each balloon is painted with a number on it represented by array nums. You are asked to burst all the balloons. If the you burst balloon i you will get nums[left] * nums[i] * nums[right] coins. Here left and right are adjacent indices of i. After the burst, the left and right then becomes adjacent.
+
+Find the maximum coins you can collect by bursting the balloons wisely.
+
+Note:
+
+You may imagine nums[-1] = nums[n] = 1. They are not real therefore you can not burst them.
+0 ¡Ü n ¡Ü 500, 0 ¡Ü nums[i] ¡Ü 100
+*/
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+	int maxCoins(vector<int>& nums) {
+
+	}
+};
+int main()
+{
+    std::cout << "Hello World!\n";
+}
