74592c19f789d6f12023d88073cf3c3359d75f9d
xavier wu <xaverwu@outlook.com>
2019-08-06 14:33:26 +0800

    completed 174

--- leetcode_174/leetcode_174.cpp
+++ leetcode_174/leetcode_174.cpp
@@ -3,22 +3,47 @@
 
 #include <iostream>
 #include <vector>
+#include <algorithm>
 using namespace std;
 
+// v1 faster than 99.19% 
+//class Solution {
+//public:
+//	int calculateMinimumHP(vector<vector<int>>& dungeon) {
+//		int N = dungeon.size();
+//		int M = dungeon[0].size();
+//		// the min hp needed at position (i, j)
+//		vector<vector<int>> hp(N + 1, vector<int>(M + 1, INT_MAX));
+//		// why 1? Because you have crossed the whold dungeon! 
+//		hp[N][M - 1] = 1;
+//		hp[N - 1][M] = 1;
+//		for (int i = N - 1; i >= 0; --i) {
+//			for (int j = M - 1; j >= 0; --j) {
+//				int need = -dungeon[i][j] + min(hp[i + 1][j], hp[i][j + 1]);
+//				hp[i][j] = need <= 0 ? 1 : need;
+//			}
+//		}
+//		return hp[0][0];
+//	}
+//};
 
+// v2 faster than 73.28%, less than 100.00% 
 class Solution {
 public:
-	int calculateHP(vector<vector<int>>& dungeon, const pair<int, int>& cur, int& curHP) {
-		curHP += dungeon[cur.first][cur.second];
-		int minHP1 = calculateHP(dungeon, make_pair(cur.first + 1, cur.second), curHP);
-		int minHP2 = calculateHP(dungeon, make_pair(cur.first, cur.second + 1), curHP);
-		curHP -= dungeon[cur.first][cur.second];
-		return fmin(minHP1, minHP2);
-	}
 	int calculateMinimumHP(vector<vector<int>>& dungeon) {
 		int N = dungeon.size();
 		int M = dungeon[0].size();
-		
+		vector<int> dp(N + 1, INT_MAX);
+		dp[N - 1] = 1;
+		// for each col
+		for (int j = M - 1; j >= 0; --j) {
+			// go up
+			for (int i = N - 1; i >= 0; --i) {
+				dp[i] = -dungeon[i][j] + min(dp[i], dp[i + 1]);
+				dp[i] = max(1, dp[i]);
+			}
+		}
+		return dp[0];
 	}
 };
 
