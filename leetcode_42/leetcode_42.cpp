57db04e210bdb1cd6003fbd5c4688737aca8d91b
wuzewei <wuzewei@autohome.com.cn>
2019-08-08 23:12:50 +0800

    update 42

--- leetcode_42/leetcode_42.cpp
+++ leetcode_42/leetcode_42.cpp
@@ -12,19 +12,36 @@ using namespace std;
 
 class Solution {
 public:
-	// v1: faster than 61.31% 
+	
 	int trap(vector<int>& height) {
+		// v1: faster than 61.31% 
+		//int len = height.size();
+		//if (len <= 2) return 0;
+		//int result = 0;
+		//vector<int> lMax(len);
+		//vector<int> rMax(len);
+		//for (int i = 1; i < len; ++i)
+		//	lMax[i] = height[i - 1] > lMax[i - 1] ? height[i - 1] : lMax[i - 1];
+		//for (int i = len - 2; i >= 0; --i)
+		//	rMax[i] = height[i + 1] > rMax[i + 1] ? height[i + 1] : rMax[i + 1];
+		//for (int i = 0; i < len; ++i) {
+		//	int tmp = min(lMax[i], rMax[i]) - height[i];
+		//	if (tmp > 0) result += tmp;
+		//}
+		//return result;
+
+		//v2: faster than 61.31%
 		int len = height.size();
 		if (len <= 2) return 0;
 		int result = 0;
-		vector<int> lMax(len);
 		vector<int> rMax(len);
-		for (int i = 1; i < len; ++i)
-			lMax[i] = height[i - 1] > lMax[i - 1] ? height[i - 1] : lMax[i - 1];
 		for (int i = len - 2; i >= 0; --i)
 			rMax[i] = height[i + 1] > rMax[i + 1] ? height[i + 1] : rMax[i + 1];
+		int lMax = 0;
 		for (int i = 0; i < len; ++i) {
-			int tmp = min(lMax[i], rMax[i]) - height[i];
+			if (i > 0 && height[i - 1] > lMax)
+				lMax = height[i - 1];
+			int tmp = min(lMax, rMax[i]) - height[i];
 			if (tmp > 0) result += tmp;
 		}
 		return result;
