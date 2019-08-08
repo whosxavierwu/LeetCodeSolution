e1bd80c44246fdceaa2d2eacbe4096a069a1b499
xavier wu <xaverwu@outlook.com>
2019-08-08 20:56:26 +0800

    update

---
+++ leetcode_42/leetcode_42.cpp
@@ -0,0 +1,41 @@
+// leetcode_42.cpp : This file contains the 'main' function. Program execution begins and ends there.
+// https://leetcode.com/problems/trapping-rain-water/
+/*
+Given n non-negative integers representing an elevation map where the width of each bar is 1, 
+compute how much water it is able to trap after raining.
+*/
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+class Solution {
+public:
+	// v1: faster than 61.31% 
+	int trap(vector<int>& height) {
+		int len = height.size();
+		if (len <= 2) return 0;
+		int result = 0;
+		vector<int> lMax(len);
+		vector<int> rMax(len);
+		for (int i = 1; i < len; ++i)
+			lMax[i] = height[i - 1] > lMax[i - 1] ? height[i - 1] : lMax[i - 1];
+		for (int i = len - 2; i >= 0; --i)
+			rMax[i] = height[i + 1] > rMax[i + 1] ? height[i + 1] : rMax[i + 1];
+		for (int i = 0; i < len; ++i) {
+			int tmp = min(lMax[i], rMax[i]) - height[i];
+			if (tmp > 0) result += tmp;
+		}
+		return result;
+	}
+};
+int main()
+{
+	Solution sol;
+	vector<int> arr = { 5,2,1,2,1,5 };
+	//vector<int> arr = { 0,1,0,2,1,0,1,3,2,1,2,1 };
+	int result = sol.trap(arr);
+	cout << result << endl;
+	return 0;
+}
---
+++ leetcode_621/leetcode_621.cpp
@@ -0,0 +1,28 @@
+// leetcode_621.cpp : This file contains the 'main' function. Program execution begins and ends there.
+// https://leetcode.com/problems/task-scheduler/
+/*
+Given a char array representing tasks CPU need to do. 
+It contains capital letters A to Z where different letters represent different tasks. 
+Tasks could be done without original order. Each task could be done in one interval. 
+For each interval, CPU could finish one task or just be idle.
+
+However, there is a non-negative cooling interval n that means between two same tasks, 
+there must be at least n intervals that CPU are doing different tasks or just be idle.
+
+You need to return the least number of intervals the CPU will take to finish all the given tasks.
+
+*/
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+	int leastInterval(vector<char>& tasks, int n) {
+
+	}
+};
+int main()
+{
+    std::cout << "Hello World!\n";
+}
