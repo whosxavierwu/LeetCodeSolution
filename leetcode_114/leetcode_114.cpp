3dc6319132eb7f1a5ec7547fca37097c68f570b4
xavier wu <xaverwu@outlook.com>
2019-08-09 13:06:25 +0800

    init projects

---
+++ leetcode_114/leetcode_114.cpp
@@ -0,0 +1,25 @@
+// leetcode_114.cpp : This file contains the 'main' function. Program execution begins and ends there.
+// https://leetcode.com/problems/flatten-binary-tree-to-linked-list/
+/*
+Given a binary tree, flatten it to a linked list in-place.
+*/
+
+#include <iostream>
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+
+class Solution {
+public:
+	void flatten(TreeNode* root) {
+
+	}
+};
+int main()
+{
+    std::cout << "Hello World!\n";
+}
