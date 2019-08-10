557d8160d861608a87cb3a58e8fdbec587ddf801
wuzewei <wuzewei@autohome.com.cn>
2019-08-10 11:14:29 +0800

    update Revert "completed 146"
    This reverts commit ff86f3e6b85d0561ea88ae0c256fb653d839271b.

---
+++ Cache/leetcode_146.cpp
@@ -0,0 +1,44 @@
+// leetcode_146.cpp : This file contains the 'main' function. Program execution begins and ends there.
+// https://leetcode.com/problems/lru-cache/
+/*
+Design and implement a data structure for Least Recently Used (LRU) cache. 
+It should support the following operations: get and put.
+
+get(key) - Get the value (will always be positive) of the key 
+if the key exists in the cache, otherwise return -1.
+
+put(key, value) - Set or insert the value if the key is not already present. 
+When the cache reached its capacity, 
+it should invalidate the least recently used item before inserting a new item.
+
+The cache is initialized with a positive capacity.
+
+Follow up:
+Could you do both operations in O(1) time complexity?
+*/
+#include <iostream>
+class LRUCache {
+public:
+	LRUCache(int capacity) {
+
+	}
+
+	int get(int key) {
+
+	}
+
+	void put(int key, int value) {
+
+	}
+};
+
+/**
+ * Your LRUCache object will be instantiated and called as such:
+ * LRUCache* obj = new LRUCache(capacity);
+ * int param_1 = obj->get(key);
+ * obj->put(key,value);
+ */
+int main()
+{
+    std::cout << "Hello World!\n";
+}
