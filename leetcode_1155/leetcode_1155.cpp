c1da72897228c9edf0b72ef666411a7e9d24ad84
xavier wu <xaverwu@outlook.com>
2019-08-12 10:37:41 +0800

    completed 1155

--- leetcode_1155/leetcode_1155.cpp
+++ leetcode_1155/leetcode_1155.cpp
@@ -8,7 +8,7 @@ using namespace std;
 
 class Solution {
 public:
-	int numRollsToTarget(int d, int f, int target) {
+	int numRollsToTarget(int numOfDice, int numOfFace, int target) {
 		// v1: TLE
 		//if (d == 1) return target <= f;
 		//long result = 0;
@@ -20,20 +20,22 @@ public:
 		//}
 		//return result % 1000000007;
 
-		// v2 todo
-		vector<vector<long>> dp(d + 1, vector<long>(target + 1));
-		for (int _f = 1; _f <= f; ++_f)
-			dp[1][_f] = 1;
-		for (int _d = 2; _d <= d; ++_d) {
-			for (int _t = _d; _t <= target; ++_t) {
-				long sum = 0;
-				for (int _f = 1; _f <= f && _f < _t; ++_f) {
-					sum += (dp[_d - 1][_t - _f]) % 1000000007;
+		// v2 faster than 100.00%
+		if (numOfDice == 1) return target <= numOfFace;
+		vector<vector<int>> dp(target + 1, vector<int>(numOfDice + 1));
+		for (int i = 1; i <= numOfFace && i <= target; ++i) {
+			dp[i][1] = 1;
+		}
+		for (int _d = 2; _d <= numOfDice; ++_d) {
+			for (int _t = 2; _t <= target; ++_t) {
+				long long sum = 0;
+				for (int _f = 1; (_f <= numOfFace) && (_f < _t); ++_f) {
+					sum += dp[_t - _f][_d - 1];
 				}
-				dp[_d][_t] = sum % 1000000007;
+				dp[_t][_d] = sum % 1000000007;
 			}
 		}
-		return dp[d][target] ;
+		return dp[target][numOfDice];
 	}
 };
 
@@ -41,14 +43,14 @@ int main()
 {
 	Solution sol;
 	int result = 0; 
-	//result = sol.numRollsToTarget(1, 6, 3);
-	//cout << result << endl;
-	//result = sol.numRollsToTarget(2, 6, 7);
-	//cout << result << endl;
-	//result = sol.numRollsToTarget(2, 5, 10);
-	//cout << result << endl;
-	//result = sol.numRollsToTarget(1, 2, 3);
-	//cout << result << endl;
+	result = sol.numRollsToTarget(1, 6, 3);
+	cout << result << endl;
+	result = sol.numRollsToTarget(2, 6, 7);
+	cout << result << endl;
+	result = sol.numRollsToTarget(2, 5, 10);
+	cout << result << endl;
+	result = sol.numRollsToTarget(1, 2, 3);
+	cout << result << endl;
 	result = sol.numRollsToTarget(30, 30, 500);
 	cout << result << endl;
 }
--- leetcode_1156/leetcode_1156.cpp
+++ leetcode_1156/leetcode_1156.cpp
@@ -1,7 +1,8 @@
 // leetcode_1156.cpp : This file contains the 'main' function. Program execution begins and ends there.
 // https://leetcode.com/problems/swap-for-longest-repeated-character-substring/
 /*
-Given a string text, we are allowed to swap two of the characters in the string. Find the length of the longest substring with repeated characters.
+Given a string text, we are allowed to swap two of the characters in the string. 
+Find the length of the longest substring with repeated characters.
 */
 
 #include <iostream>
