aa9ea01bb2d45d9f8c91e30166ac3fdcf3518120
xavier wu <xaverwu@outlook.com>
2019-08-16 13:40:01 +0800

    completed 98

--- leetcode_98/leetcode_98.cpp
+++ leetcode_98/leetcode_98.cpp
@@ -9,7 +9,6 @@ The left subtree of a node contains only nodes with keys less than the node's ke
 The right subtree of a node contains only nodes with keys greater than the node's key.
 Both the left and right subtrees must also be binary search trees.
 
-
 Example 1:
 
 	2
@@ -32,6 +31,8 @@ Explanation: The root node's value is 5 but its right child's value is 4.
 */
 
 #include <iostream>
+#include <stack>
+using namespace std;
 
 
 struct TreeNode {
@@ -41,13 +42,72 @@ struct TreeNode {
     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
 };
 
+// v1: faster than 63.02%
+//class Solution {
+//public:
+//	bool isValidBST(TreeNode* root, long low, long high) {
+//		if (root == NULL) return true;
+//		if (root->val <= low || root->val >= high) return false;
+//		if (root->left != NULL && root->left->val >= root->val) return false;
+//		if (root->right != NULL && root->val >= root->right->val) return false;
+//		return ((root->left == NULL) || isValidBST(root->left, low, root->val))
+//			&& ((root->right == NULL) || isValidBST(root->right, root->val, high));
+//	}
+//	bool isValidBST(TreeNode* root) { 
+//		return isValidBST(root, LONG_MIN, LONG_MAX);
+//	}
+//};
+
+// v2: faster than 63.02%
+//class Solution {
+//public:
+//	bool isValidBST(TreeNode* root, pair<long, long> bound) {
+//		if (root == NULL) return true;
+//		if (root->val <= bound.first || root->val >= bound.second) return false;
+//		if (root->left != NULL && root->left->val >= root->val) return false;
+//		if (root->right != NULL && root->val >= root->right->val) return false;
+//		if (!isValidBST(root->left, { bound.first, root->val })) return false;
+//		if (!isValidBST(root->right, { root->val, bound.second })) return false;
+//		return true;
+//	}
+//	bool isValidBST(TreeNode* root) {
+//		return isValidBST(root, { LONG_MIN, LONG_MAX });
+//	}
+//};
+
+// v3: faster than 98.50%, less than 97.92% 
 class Solution {
 public:
 	bool isValidBST(TreeNode* root) {
-
+		if (!root) return true;
+		stack<pair<TreeNode*, pair<long, long>>> sta;
+		sta.push({ root, {LONG_MIN, LONG_MAX} });
+		while (!sta.empty()) {
+			pair<TreeNode*, pair<long, long>> cur = sta.top();
+			sta.pop();
+			TreeNode* root = cur.first;
+			long low = cur.second.first;
+			long high = cur.second.second;
+			if (root->val <= low || root->val >= high) return false;
+			if (root->right != NULL) {
+				if (root->val >= root->right->val) return false;
+				sta.push({ root->right, {root->val, high} });
+			}
+			if (root->left != NULL) {
+				if (root->left->val >= root->val) return false;
+				sta.push({ root->left, {low, root->val} });
+			}
+		}
+		return true;
 	}
 };
+
 int main()
 {
-    std::cout << "Hello World!\n";
+	Solution sol; 
+	TreeNode* root = new TreeNode(2);
+	root->left = new TreeNode(1);
+	root->right = new TreeNode(3);
+	bool result = sol.isValidBST(root);
+	cout << result << endl;
 }
