e88ff2fdbfa0cb7bdb7e63c301d4eff98573157c
wuzewei <wuzewei@autohome.com.cn>
2019-08-19 22:16:24 +0800

    completed 560

--- leetcode_279/leetcode_279.cpp
+++ leetcode_279/leetcode_279.cpp
@@ -1,14 +1,13 @@
 // leetcode_279.cpp : This file contains the 'main' function. Program execution begins and ends there.
 // todo https://leetcode.com/problems/perfect-squares/
 /*
-Given a positive integer n, find the least number of perfect square numbers (for example, 1, 4, 9, 16, ...) which sum to n.
-
-Example 1:
+Given a positive integer n, 
+find the least number of perfect square numbers 
+(for example, 1, 4, 9, 16, ...) which sum to n.
 
 Input: n = 12
 Output: 3
 Explanation: 12 = 4 + 4 + 4.
-Example 2:
 
 Input: n = 13
 Output: 2
--- leetcode_560/leetcode_560.cpp
+++ leetcode_560/leetcode_560.cpp
@@ -1,27 +1,51 @@
 // leetcode_560.cpp : This file contains the 'main' function. Program execution begins and ends there.
-// todo https://leetcode.com/problems/subarray-sum-equals-k/
+// https://leetcode.com/problems/subarray-sum-equals-k/
 /*
-Given an array of integers and an integer k, you need to find the total number of continuous subarrays whose sum equals to k.
+Given an array of integers and an integer k, 
+you need to find the total number of continuous subarrays whose sum equals to k.
 
-Example 1:
 Input:nums = [1,1,1], k = 2
 Output: 2
-Note:
-The length of the array is in range [1, 20,000].
-The range of numbers in the array is [-1000, 1000] and the range of the integer k is [-1e7, 1e7].
 */
+
 #include <iostream>
 #include <vector>
+#include <unordered_map>
 using namespace std;
 
 class Solution {
 public:
 	int subarraySum(vector<int>& nums, int k) {
+		// v1: faster than 18.47% 
+		//int len = nums.size();
+		//int result = 0;
+		//for (int i = 0; i < len; ++i) {
+		//	for (int j = i, sum = 0; j < len; ++j) {
+		//		sum += nums[j];
+		//		if (sum == k)
+		//			++result;
+		//	}
+		//}
+		//return result;
 
+		// v2:faster than 89.75% 
+		unordered_map<int, int> mmap;
+		int count = 0, sum = 0, len = nums.size();
+		mmap[0] = 1;
+		for (int i = 0; i < len; ++i) {
+			sum += nums[i];
+			if (mmap.find(sum - k) != mmap.end())
+				count += mmap[sum - k];
+			mmap[sum] += 1;
+		}
+		return count;
 	}
 };
+
 int main()
 {
-    std::cout << "Hello World!\n";
+	Solution sol; 
+	vector<int> nums = { 1, 1, 1 };
+	cout << sol.subarraySum(nums, 2) << endl;
 }
 
