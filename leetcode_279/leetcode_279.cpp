7a856ce3e9be1913fda7b26505f199cef135818b
xavier wu <xaverwu@outlook.com>
2019-08-21 20:54:34 +0800

    completed 279

--- leetcode_279/leetcode_279.cpp
+++ leetcode_279/leetcode_279.cpp
@@ -14,10 +14,59 @@ Output: 2
 Explanation: 13 = 4 + 9.
 */
 #include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
 class Solution {
 public:
+	int is_square(int n) {
+		int sqrt_n = (int)sqrt(n);
+		return (sqrt_n * sqrt_n == n);
+	}
 	int numSquares(int n) {
+		// v1: 104 ms, faster than 54.69% 
+		//if (n <= 0) return 0;
+		//vector<int> dp(n + 1, INT_MAX);
+		//dp[0] = 0;
+		//for (int i = 1; i <= n; ++i) {
+		//	for (int j = 1; j * j <= i; ++j) {
+		//		dp[i] = min(dp[i], dp[i - j * j] + 1);
+		//	}
+		//}
+		//return dp.back();
 
+		// v2: 8 ms, faster than 90.37%
+		//if (n <= 0) return 0;
+		//static vector<int> dp({ 0 });
+		//while (dp.size() <= n) {
+		//	int m = dp.size();
+		//	int cnt = INT_MAX;
+		//	for (int i = 1; i * i <= m; ++i)
+		//		cnt = min(cnt, dp[m - i * i] + 1);
+		//	dp.push_back(cnt);
+		//}
+		//return dp[n];
+
+		// v3  0 ms, faster than 100.00% 
+		// Based on Lagrange's Four Square theorem, there are only 4 possible results: 1, 2, 3, 4.
+		if (is_square(n)) return 1;
+		// The result is 4 if and only if n can be written in the 
+		// form of 4^k*(8*m + 7). Please refer to 
+		// Legendre's three-square theorem.
+		while ((n & 3) == 0) {  // n%4==0
+			n >>= 2;
+		}
+		if ((n & 7) == 7) {  // // n%8 == 7
+			return 4;
+		}
+		int sqrt_n = (int)sqrt(n);
+		for (int i = 1; i <= sqrt_n; ++i) {
+			if (is_square(n - i * i)) {
+				return 2;
+			}
+		}
+		return 3;
 	}
 };
 int main()
