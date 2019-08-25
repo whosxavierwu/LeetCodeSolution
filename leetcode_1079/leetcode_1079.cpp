bb3d0675c5feb5efe4d60fc92769282fa49e9042
wuzewei <wuzewei@autohome.com.cn>
2019-08-25 09:59:18 +0800

    update

--- leetcode_1079/leetcode_1079.cpp
+++ leetcode_1079/leetcode_1079.cpp
@@ -5,51 +5,53 @@ You have a set of tiles,
 where each tile has one letter tiles[i] printed on it.  
 Return the number of possible non-empty sequences of letters you can make.
 */
+
 #include <iostream>
 #include <string>
 #include <vector>
 #include <unordered_map>
+#include <algorithm>
 using namespace std;
 
+// v1: https://leetcode.com/problems/letter-tile-possibilities/discuss/308387/c%2B%2B-0ms-8.5-MB-beats-100-(both-time-and-memory)-with-algo
+// 4 ms, faster than 94.20%
 class Solution {
-public:
-	int insertR2N(int n, int r) {
-		// insert r numbers to length n (n+1 slots)
-		if (n == 0) return 1;
-		if (n == 1) return r + 1;
-		if (r == 1) return n + 1;
-		// put r numbers to n+1 slots
-		// f(n+1, r) = f(n, r) + f(n, r-1) + ... + f(n, 0)
-		vector<int> f(r + 1, 1);
-		for (int i = 2; i <= n + 1; ++i) {
-			for (int j = 1; j <= r; ++j) {
-				f[j] += f[j - 1];
-			}
-		}
-		return f[r];
+	int factorial(int n) {
+		int fact = 1; 
+		for (int i = 2; i <= n; ++i)
+			fact *= i;
+		return fact;
 	}
-	int numTilePossibilities(string tiles) {
-		vector<int> letterCount(26);
-		for (char& c : tiles) letterCount[c - 'A'] += 1;
-		// number of results in length i (1 to n)
-		vector<int> numOfPermuteGivenLen(tiles.length() + 1); 
-		
-		int prevMaxLen = 0;
-		for (int& curLetterCount : letterCount) {
-			for (int numOfLettersToInsert = 1; numOfLettersToInsert <= curLetterCount; ++numOfLettersToInsert) {
-				for (int prevLen = 1; prevLen <= prevMaxLen; ++prevLen) {
-					//int tmp = numOfPermuteGivenLen[prevLen + numOfLettersToInsert];
-					//if (tmp == 0) tmp = 1;
-					//numOfPermuteGivenLen[prevLen + numOfLettersToInsert] = tmp * insertR2N(prevLen, numOfLettersToInsert);
-					numOfPermuteGivenLen[prevLen + numOfLettersToInsert] = numOfPermuteGivenLen[prevLen] * insertR2N(prevLen, numOfLettersToInsert);
-				}
-				numOfPermuteGivenLen[numOfLettersToInsert] += 1;
-			}
-			prevMaxLen += curLetterCount;
+	int countDistinctPerm(string str) {
+		int len = str.length();
+		vector<int> freq(26);
+		for (int i = 0; i < len; ++i) 
+			if (str[i] >= 'A')
+				freq[str[i] - 'A']++;
+		int fact = 1;
+		for (int i = 0; i < 26; ++i)
+			fact *= factorial(freq[i]);
+		return factorial(len) / fact;
+	}
+	void findComb(const string& tiles, string s, int i, int index, int& result) {
+		if (index == s.length()) {
+			result += countDistinctPerm(s);
+			return;
 		}
+		int n = tiles.size();
+		if (i >= n) return;
+		s[index] = tiles[i];
+		findComb(tiles, s, i + 1, index + 1, result);
+		while (i < n - 1 && tiles[i] == tiles[i + 1]) ++i;
+		findComb(tiles, s, i + 1, index, result);
+	}
+public:
+	int numTilePossibilities(string tiles) {
 		int result = 0;
-		for (int& a : numOfPermuteGivenLen) {
-			result += a;
+		sort(tiles.begin(), tiles.end());
+		for (int curNumOfTile = 1; curNumOfTile <= tiles.length(); ++curNumOfTile) {
+			string s(curNumOfTile, '.');
+			findComb(tiles, s, 0, 0, result);
 		}
 		return result;
 	}
