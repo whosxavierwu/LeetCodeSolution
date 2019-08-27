e8cede86dd1b42c2ee04b277025605bd194a473a
xavier wu <xaverwu@outlook.com>
2019-08-27 12:35:03 +0800

    completed 416

--- leetcode_416/leetcode_416.cpp
+++ leetcode_416/leetcode_416.cpp
@@ -1,20 +1,52 @@
 // leetcode_416.cpp : This file contains the 'main' function. Program execution begins and ends there.
-// todo https://leetcode.com/problems/partition-equal-subset-sum/
+// https://leetcode.com/problems/partition-equal-subset-sum/
+/*
+Given a non-empty array containing only positive integers, 
+find if the array can be partitioned into two subsets 
+such that the sum of elements in both subsets is equal.
 
+Note:
+
+Each of the array element will not exceed 100.
+The array size will not exceed 200.
+*/
 #include <iostream>
+#include <vector>
+#include <bitset>
+using namespace std;
+
+
+class Solution {
+public:
+	bool canPartition(vector<int>& nums) {
+		// v1:  12 ms, faster than 87.07%, 8.2 MB, less than 100.00% 
+		const int max_value = 100;
+		const int max_len = 200;
+		bitset<max_value* max_len / 2 + 1> bits(1);  // bits[i] marks if there is a sum=i
+		int sum = 0;
+		for (int& num : nums) {
+			sum += num;
+			bits |= bits << num;
+		}
+		return (sum % 2 == 0) && bits[sum / 2];
+
+		// v2  156 ms, faster than 32.39% 
+		//int sum = 0;
+		//for (int num : nums) sum += num;
+		//if (sum % 2) return false;
+		//int target = sum / 2;
+		//vector<bool> dp(target + 1);
+		//dp[0] = true;
+		//for (int num : nums) 
+		//	for (int i = target; i >= num; --i) 
+		//		dp[i] = dp[i] || dp[i - num];
+		//return dp[target];
+	}
+};
 
 int main()
 {
     std::cout << "Hello World!\n";
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
 
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
