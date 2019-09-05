2edb64f054df2aa3146057aa366cf0005ce0296f
xavier wu <xaverwu@outlook.com>
2019-09-05 15:31:25 +0800

    update, add two problems

---
+++ leetcode_139/leetcode_139.cpp
@@ -0,0 +1,36 @@
+// leetcode_139.cpp : This file contains the 'main' function. Program execution begins and ends there.
+// todo https://leetcode.com/problems/word-break/
+/*
+Given a non-empty string s and a dictionary wordDict containing a list of non-empty words, determine if s can be segmented into a space-separated sequence of one or more dictionary words.
+
+The same word in the dictionary may be reused multiple times in the segmentation.
+You may assume the dictionary does not contain duplicate words.
+
+Input: s = "leetcode", wordDict = ["leet", "code"]
+Output: true
+Explanation: Return true because "leetcode" can be segmented as "leet code".
+
+Input: s = "applepenapple", wordDict = ["apple", "pen"]
+Output: true
+Explanation: Return true because "applepenapple" can be segmented as "apple pen apple".
+			 Note that you are allowed to reuse a dictionary word.
+
+Input: s = "catsandog", wordDict = ["cats", "dog", "sand", "and", "cat"]
+Output: false
+*/
+
+#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+	bool wordBreak(string s, vector<string>& wordDict) {
+
+	}
+};
+int main()
+{
+    std::cout << "Hello World!\n";
+}
---
+++ leetcode_300/leetcode_300.cpp
@@ -0,0 +1,32 @@
+// leetcode_300.cpp : This file contains the 'main' function. Program execution begins and ends there.
+// todo https://leetcode.com/problems/longest-increasing-subsequence/
+/*
+Given an unsorted array of integers, find the length of longest increasing subsequence.
+
+Example:
+
+Input: [10,9,2,5,3,7,101,18]
+Output: 4
+Explanation: The longest increasing subsequence is [2,3,7,101], therefore the length is 4.
+Note:
+
+There may be more than one LIS combination, it is only necessary for you to return the length.
+Your algorithm should run in O(n2) complexity.
+Follow up: Could you improve it to O(n log n) time complexity?
+*/
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+	int lengthOfLIS(vector<int>& nums) {
+
+	}
+};
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
