d296b18ff2e07e822b24f0d4a011768cab6fc3de
wuzewei <wuzewei@autohome.com.cn>
2019-09-25 22:15:29 +0800

    update

---
+++ N_ary_Tree/N_ary_Tree.cpp
@@ -0,0 +1,9 @@
+// N_ary_Tree.cpp : This file contains the 'main' function. Program execution begins and ends there.
+// todo n-ary tree
+
+#include <iostream>
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
