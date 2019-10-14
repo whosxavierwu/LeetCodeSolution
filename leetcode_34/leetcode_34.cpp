c95db85d9690e40ef122fc88c6e0f10c051fb7da
xavier wu <xaverwu@outlook.com>
2019-10-14 15:49:02 +0800

    completed #33

--- leetcode_34/leetcode_34.cpp
+++ leetcode_34/leetcode_34.cpp
@@ -16,15 +16,36 @@ Output: [-1,-1]
 */
 #include <iostream>
 #include <vector>
+#include <algorithm>
 using namespace std;
 
 class Solution {
 public:
 	vector<int> searchRange(vector<int>& nums, int target) {
-
+		int len = nums.size();
+		int left = 0, right = len - 1;
+		vector<int> result = { -1, -1 };
+		while (left <= right) {
+			int mid = left + (right - left) / 2;
+			if (target == nums[mid]) {
+				result[0] = mid;
+				break;
+			}
+			else if (target < nums[mid])
+				right = mid - 1;
+			else
+				left = mid + 1;
+		}
+		return result;
 	}
 };
+
 int main()
 {
-    std::cout << "Hello World!\n";
+	Solution sol;
+	vector<int> nums = { 5,7,7,8,8,10 };
+	int target = 8;
+	vector<int> result = sol.searchRange(nums, target);
+	for (int a : result) cout << a << " ";
+	cout << endl;
 }
