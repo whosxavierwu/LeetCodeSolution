6de5655eac14df6e38ee72480e854279b39f59f8
wuzewei <wuzewei@autohome.com.cn>
2019-11-24 16:50:59 +0800

    complete the final question in weekly contest 164

--- leetcode_5274/leetcode_5274.cpp
+++ leetcode_5274/leetcode_5274.cpp
@@ -1,43 +1,40 @@
 // leetcode_5274.cpp : This file contains the 'main' function. Program execution begins and ends there.
-// todo https://leetcode.com/contest/weekly-contest-164/problems/number-of-ways-to-stay-in-the-same-place-after-some-steps/
+// https://leetcode.com/contest/weekly-contest-164/problems/number-of-ways-to-stay-in-the-same-place-after-some-steps/
 
 #include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
 
 class Solution {
 public:
 	int numWays(int steps, int arrLen) {
-
+		const int MOD = 1e9 + 7;
+		if (steps < arrLen) arrLen = steps;
+		// [i][j] means take exactly i steps to get to the position j
+		vector<vector<long long>> waysToSomePlace(steps + 1, vector<long long>(arrLen, 0));
+		waysToSomePlace[0][0] = 1;
+		for (int step = 0; step < steps; ++step) {
+			for (int i = 0; i < arrLen; ++i) {
+				// go left
+				if (i > 0) {
+					waysToSomePlace[step + 1][i - 1] += waysToSomePlace[step][i];
+					waysToSomePlace[step + 1][i - 1] %= MOD;
+				}
+				// go right
+				if (i + 1 < arrLen) {
+					waysToSomePlace[step + 1][i + 1] += waysToSomePlace[step][i];
+					waysToSomePlace[step + 1][i + 1] %= MOD;
+				}
+				// stay
+				waysToSomePlace[step + 1][i] += waysToSomePlace[step][i];
+				waysToSomePlace[step + 1][i] %= MOD;
+			}
+		}
+		return waysToSomePlace[steps][0];
 	}
 };
 
-//const int MOD = 1e9 + 7;
-//
-//class Solution {
-//public:
-//	int numWays(int steps, int arrLen) {
-//		arrLen = min(arrLen, steps);
-//		vector<vector<long long>> dp(steps + 1, vector<long long>(arrLen, 0));
-//		dp[0][0] = 1;
-//
-//		for (int s = 0; s < steps; s++)
-//			for (int i = 0; i < arrLen; i++) {
-//				if (i > 0) {
-//					dp[s + 1][i - 1] += dp[s][i];
-//					dp[s + 1][i - 1] %= MOD;
-//				}
-//
-//				if (i + 1 < arrLen) {
-//					dp[s + 1][i + 1] += dp[s][i];
-//					dp[s + 1][i + 1] %= MOD;
-//				}
-//
-//				dp[s + 1][i] += dp[s][i];
-//				dp[s + 1][i] %= MOD;
-//			}
-//
-//		return dp[steps][0];
-//	}
-//};
 
 int main()
 {
