efb1f8763af24e2808388d4a2e93e598714d29f1
xavier wu <xaverwu@outlook.com>
2020-01-19 17:15:55 +0800

    update

--- leetcode_4/leetcode_4.cpp
+++ leetcode_4/leetcode_4.cpp
@@ -1,5 +1,5 @@
 // leetcode_4.cpp : This file contains the 'main' function. Program execution begins and ends there.
-// todo https://leetcode.com/problems/median-of-two-sorted-arrays/
+// https://leetcode.com/problems/median-of-two-sorted-arrays/
 /*
 There are two sorted arrays nums1 and nums2 of size m and n respectively.
 
@@ -23,12 +23,32 @@ The median is (2 + 3)/2 = 2.5
 */
 #include <iostream>
 #include <vector>
+#include <algorithm>
 using namespace std;
 
 class Solution {
 public:
     double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
-
+        // 16 ms, faster than 90.98%
+        int N1 = nums1.size(), N2 = nums2.size();
+        if (N1 < N2)  // make sure nums2 is the shorter one
+            return findMedianSortedArrays(nums2, nums1);
+        int left = 0, right = N2 * 2;
+        while (left <= right) {
+            int cut2 = (left + right) / 2;
+            int cut1 = N1 + N2 - cut2;
+            double L1 = (cut1 == 0) ? INT_MIN : nums1[(cut1 - 1) / 2];
+            double L2 = (cut2 == 0) ? INT_MIN : nums2[(cut2 - 1) / 2];
+            double R1 = (cut1 == N1 * 2) ? INT_MAX : nums1[cut1 / 2];
+            double R2 = (cut2 == N2 * 2) ? INT_MAX : nums2[cut2 / 2];
+            if (L1 > R2)
+                left = cut2 + 1;
+            else if (L2 > R1)
+                right = cut2 - 1;
+            else
+                return (max(L1, L2) + min(R1, R2)) / 2;
+        }
+        return -1;
     }
 };
 
