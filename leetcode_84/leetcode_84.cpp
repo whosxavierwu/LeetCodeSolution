25059d9adb8279e1840c7ef644a8717c6171dc93
xavier wu <xaverwu@outlook.com>
2020-01-20 15:57:37 +0800

    complete severval questions

--- leetcode_84/leetcode_84.cpp
+++ leetcode_84/leetcode_84.cpp
@@ -1,19 +1,42 @@
 // leetcode_84.cpp : This file contains the 'main' function. Program execution begins and ends there.
-// todo https://leetcode.com/problems/largest-rectangle-in-histogram/
+// https://leetcode.com/problems/largest-rectangle-in-histogram/
 /*
-Given n non-negative integers representing the histogram's bar height where the width of each bar is 1, find the area of largest rectangle in the histogram
+Given n non-negative integers representing the histogram's bar height where the width of each bar is 1, 
+find the area of largest rectangle in the histogram
 Input: [2,1,5,6,2,3]
 Output: 10
-
 */
 #include <iostream>
 #include <vector>
+#include <algorithm>
 using namespace std;
 
 class Solution {
 public:
     int largestRectangleArea(vector<int>& heights) {
-
+        // v1 12 ms, faster than 88.86%
+        if (heights.empty()) return 0;
+        int n = heights.size();
+        vector<int> lowerAtLeft(n);
+        vector<int> lowerAtRight(n);
+        lowerAtRight[n - 1] = n;
+        lowerAtLeft[0] = -1;
+        for (int i = 1; i < n; ++i) {
+            int left = i - 1;
+            while (left >= 0 && heights[left] >= heights[i])
+                left = lowerAtLeft[left];
+            lowerAtLeft[i] = left;
+        }
+        for (int i = n - 2; i >= 0; --i) {
+            int right = i + 1;
+            while (right < n && heights[right] >= heights[i])
+                right = lowerAtRight[right];
+            lowerAtRight[i] = right;
+        }
+        int result = 0;
+        for (int i = 0; i < n; ++i)
+            result = max(result, (lowerAtRight[i] - lowerAtLeft[i] - 1) * heights[i]);
+        return result;
     }
 };
 
--- leetcode_85/leetcode_85.cpp
+++ leetcode_85/leetcode_85.cpp
@@ -1,5 +1,5 @@
 // leetcode_85.cpp : This file contains the 'main' function. Program execution begins and ends there.
-// todo https://leetcode.com/problems/maximal-rectangle/
+// https://leetcode.com/problems/maximal-rectangle/
 /*
 Given a 2D binary matrix filled with 0's and 1's, find the largest rectangle containing only 1's and return its area.
 
@@ -18,12 +18,78 @@ Output: 6
 
 #include <iostream>
 #include <vector>
+#include <algorithm>
 using namespace std;
 
 class Solution {
 public:
     int maximalRectangle(vector<vector<char>>& matrix) {
+        //// v1: 24 ms, faster than 74.43%
+        //if (matrix.empty()) return 0;
+        //int N = matrix.size(); 
+        //int M = matrix[0].size();
+        //vector<int> left(M, 0);
+        //vector<int> right(M, M);
+        //vector<int> height(M, 0);
+        //int result = 0;
+        //for (int i = 0; i < N; ++i) {
+        //    int cur_left = 0, cur_right = M;
+        //    // height 
+        //    for (int j = 0; j < M; ++j)
+        //        height[j] = (matrix[i][j] == '1') ? (height[j] + 1) : 0;
+        //    // left 
+        //    for (int j = 0; j < M; ++j)
+        //        if (matrix[i][j] == '1')
+        //            left[j] = max(left[j], cur_left);
+        //        else {
+        //            left[j] = 0;
+        //            cur_left = j + 1;  // left to right, find the last non-zero
+        //        }
+        //    // right 
+        //    for (int j = M - 1; j >= 0; --j)
+        //        if (matrix[i][j] == '1')
+        //            right[j] = min(right[j], cur_right);
+        //        else {
+        //            right[j] = M;
+        //            cur_right = j;  // right to left, find the last 0
+        //        }
+        //    for (int j = 0; j < M; ++j)
+        //        result = max(result, (right[j] - left[j]) * height[j]);
+        //}
+        //return result;
 
+        // v2 20 ms, faster than 92.78%
+        if (matrix.empty()) return 0;
+        int N = matrix.size();
+        int M = matrix[0].size();
+        vector<int> left(M, 0);
+        vector<int> right(M, M);
+        vector<int> height(M, 0);
+        int result = 0;
+        for (int i = 0; i < N; ++i) {
+            int cur_left = 0, cur_right = M;
+            for (int j = 0; j < M; ++j) {
+                if (matrix[i][j] == '1') {
+                    height[j]++;
+                    left[j] = max(left[j], cur_left);
+                }
+                else {
+                    height[j] = 0;
+                    left[j] = 0;
+                    cur_left = j + 1;
+                }
+                if (matrix[i][M - 1 - j] == '1') {
+                    right[M - 1 - j] = min(right[M - 1 - j], cur_right);
+                }
+                else {
+                    right[M - 1 - j] = M;
+                    cur_right = M - 1 - j;
+                }
+            }
+            for (int j = 0; j < M; ++j)
+                result = max(result, (right[j] - left[j]) * height[j]);
+        }
+        return result;
     }
 };
 
