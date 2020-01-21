1b6c5fdd8d6ebc80d88ab3f4eb82e9d839d35684
xavier wu <xaverwu@outlook.com>
2020-01-21 16:41:35 +0800

    complete 34 & 55

--- leetcode_34/leetcode_34.cpp
+++ leetcode_34/leetcode_34.cpp
@@ -1,5 +1,5 @@
 // leetcode_34.cpp : This file contains the 'main' function. Program execution begins and ends there.
-// todo https://leetcode.com/problems/find-first-and-last-position-of-element-in-sorted-array/
+// https://leetcode.com/problems/find-first-and-last-position-of-element-in-sorted-array/
 /*
 Given an array of integers nums sorted in ascending order, 
 find the starting and ending position of a given target value.
@@ -16,27 +16,54 @@ Output: [-1,-1]
 */
 #include <iostream>
 #include <vector>
-#include <algorithm>
 using namespace std;
 
 class Solution {
 public:
-	vector<int> searchRange(vector<int>& nums, int target) {
-		int len = nums.size();
-		int left = 0, right = len - 1;
-		vector<int> result = { -1, -1 };
+	int searchLowBound(const vector<int>& nums, int target) {
+		int left = 0, right = nums.size() - 1;
 		while (left <= right) {
 			int mid = left + (right - left) / 2;
-			if (target == nums[mid]) {
-				result[0] = mid;
-				break;
-			}
-			else if (target < nums[mid])
-				right = mid - 1;
-			else
+			if (nums[mid] < target)
 				left = mid + 1;
+			else
+				right = mid - 1;
 		}
-		return result;
+		return left;
+	}
+	vector<int> searchRange(vector<int>& nums, int target) {
+		// v1: 12 ms, faster than 35.50% 
+		//if (nums.empty()) return { -1, -1 };
+		//int left = 0, right = nums.size() - 1;
+		//vector<int> result(2, -1);
+		//while (left < right) {
+		//	int mid = left + (right - left) / 2;
+		//	if (nums[mid] < target)
+		//		left = mid + 1;
+		//	else
+		//		right = mid;
+		//}
+		//if (nums[left] != target)
+		//	return result;
+		//result[0] = left;
+		//right = nums.size() - 1;
+		//while (left < right) {
+		//	int mid = left + (right - left) / 2 + 1;
+		//	if (nums[mid] > target)
+		//		right = mid - 1;
+		//	else
+		//		left = mid;
+		//}
+		//result[1] = right;
+		//return result;
+
+		// v2:  8 ms, faster than 85.29% 
+		int idx1 = searchLowBound(nums, target);
+		int idx2 = searchLowBound(nums, target + 1) - 1;
+		if (idx1 <= idx2)
+			return { idx1, idx2 };
+		else
+			return { -1, -1 };
 	}
 };
 
--- leetcode_55/leetcode_55.cpp
+++ leetcode_55/leetcode_55.cpp
@@ -1,33 +1,67 @@
 // leetcode_55.cpp : This file contains the 'main' function. Program execution begins and ends there.
-// todo https://leetcode.com/problems/jump-game/
+// https://leetcode.com/problems/jump-game/
 /*
 Given an array of non-negative integers, you are initially positioned at the first index of the array.
-
 Each element in the array represents your maximum jump length at that position.
-
 Determine if you are able to reach the last index.
-
-Example 1:
 
 Input: [2,3,1,1,4]
 Output: true
 Explanation: Jump 1 step from index 0 to 1, then 3 steps to the last index.
-Example 2:
 
 Input: [3,2,1,0,4]
 Output: false
 Explanation: You will always arrive at index 3 no matter what. Its maximum
              jump length is 0, which makes it impossible to reach the last index.
-
 */
+
 #include <iostream>
 #include <vector>
+#include <queue>
+#include <stack>
 using namespace std;
 
 class Solution {
 public:
     bool canJump(vector<int>& nums) {
+        // v1: TLE
+        //int n = nums.size();
+        //vector<bool> canReach(n, false);
+        //canReach[0] = true;
+        //for (int i = 0; i < n; ++i) {
+        //    if (canReach[i]) {
+        //        for (int j = 1; j <= nums[i]; ++j)
+        //            canReach[i + j] = true;
+        //    }
+        //}
+        //return canReach[n - 1];
 
+        // v2: TLE
+        //int n = nums.size();
+        //vector<bool> visited(n, false);
+        //stack<int> sta;
+        //sta.push(0);
+        //while (!sta.empty()) {
+        //    int top = sta.top();
+        //    if (top == n - 1) 
+        //        return true;
+        //    sta.pop();
+        //    visited[top] = true;
+        //    for (int i = 1; i <= nums[top] && (top + i) < n; ++i)
+        //        if (!visited[top + i])
+        //            sta.push(top + i);
+        //}
+        //return false;
+
+        // v3: 12 ms, faster than 72.24%
+        int n = nums.size();
+        int maxIdx = 0;
+        for (int i = 0; i < n; ++i) {
+            if (i > maxIdx)
+                return false;
+            maxIdx = max(maxIdx, i + nums[i]);
+        }
+        return true;
     }
 };
 
