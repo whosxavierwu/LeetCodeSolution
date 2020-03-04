65fd6766d6fd77ff62513642db4d1f3c4a75d3fa
xavier wu <xaverwu@outlook.com>
2020-03-04 18:03:52 +0800

    update kthlargest & topkfreq

--- SearchInArray/leetcode_215.cpp
+++ SearchInArray/leetcode_215.cpp
@@ -24,6 +24,57 @@ public:
 	}
 };
 
+
+class Solution {
+	int partition(vector<int>& nums, int left, int right) {
+		int pivot = nums[left];
+		int l = left + 1, r = right;
+		while (l <= r) {
+			if (nums[l] < pivot && nums[r] > pivot) {
+				swap(nums[l], nums[r]);
+				++l;
+				--r;
+			}
+			if (nums[l] >= pivot)
+				++l;
+			if (nums[r] <= pivot)
+				--r;
+		}
+		swap(nums[left], nums[r]);
+		return r;
+	}
+public:
+	int findKthLargest(vector<int>& nums, int k) {
+		// v1 sort faster than 80.23% 
+		//sort(nums.begin(), nums.end());
+		//return nums[nums.size() - k];
+
+		// v2 faster than 97.71% 
+		//priority_queue<int> pq;
+		//for (int num : nums)
+		//	pq.push(num);
+		//for (int i = 1; i < k; ++i)
+		//	pq.pop();
+		//return pq.top();
+
+		// v3: 80 ms, faster than 10.47%
+		int left = 0, right = nums.size() - 1, kth;
+		while (true) {
+			int idx = partition(nums, left, right);
+			if (idx == k - 1) {
+				kth = nums[idx];
+				break;
+			}
+			if (idx < k - 1)
+				left = idx + 1;
+			else
+				right = idx - 1;
+		}
+		return kth;
+	}
+};
+
+
 int main()
 {
     std::cout << "Hello World!\n";
--- SearchInArray/leetcode_347.cpp
+++ SearchInArray/leetcode_347.cpp
@@ -13,39 +13,38 @@ class Solution {
 public:
 	vector<int> topKFrequent(vector<int>& nums, int k) {
 		// v1: faster than 82.19%
-		//int len = nums.size();
-		//unordered_map<int, int> cntMap;
-		//for (int num : nums)
-		//	cntMap[num]++;
-		//// pair: frequency and number
-		//priority_queue<pair<int, int>> pq;
-		//vector<int> result;
-		//for (auto iter = cntMap.begin(); iter != cntMap.end(); ++iter) {
-		//	pq.push(make_pair(iter->second, iter->first));
-		//	if (pq.size() > cntMap.size() - k) {  // why?
-		//		result.push_back(pq.top().second);
-		//		pq.pop();
-		//	}	
-		//}
-		//return result;
+		int len = nums.size();
+		unordered_map<int, int> cntMap;
+		for (int num : nums)
+			cntMap[num]++;
+		priority_queue<pair<int, int>> pq;  // pair: frequency and number
+		vector<int> result;
+		for (auto iter = cntMap.begin(); iter != cntMap.end(); ++iter) {
+			pq.push(make_pair(iter->second, iter->first));
+			if (pq.size() > cntMap.size() - k) {  // why?
+				result.push_back(pq.top().second);
+				pq.pop();
+			}	
+		}
+		return result;
 
 		// v2 faster than 39.57%
 		// the greatest one is on the top
-		priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
-		unordered_map<int, int> cntMap;
-		for (int n : nums)
-			cntMap[n]++;
-		for (auto iter = cntMap.begin(); iter != cntMap.end(); ++iter) {
-			pq.push(make_pair(iter->second, iter->first));
-			while (pq.size() > k)  // why?
-				pq.pop();
-		}
-		vector<int> result;
-		while (!pq.empty()) {
-			result.push_back(pq.top().second);
-			pq.pop();
-		}
-		return result;
+		//priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
+		//unordered_map<int, int> cntMap;
+		//for (int n : nums)
+		//	cntMap[n]++;
+		//for (auto iter = cntMap.begin(); iter != cntMap.end(); ++iter) {
+		//	pq.push(make_pair(iter->second, iter->first));
+		//	while (pq.size() > k)  // why?
+		//		pq.pop();
+		//}
+		//vector<int> result;
+		//while (!pq.empty()) {
+		//	result.push_back(pq.top().second);
+		//	pq.pop();
+		//}
+		//return result;
 	}
 };
 
