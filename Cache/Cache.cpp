94b6924060a4d0c2c88cbe0ebf29ead55e670d3e
xavier wu <whosxavierwu@gmail.com>
2020-03-07 16:27:59 +0800

    done practicing LRU

--- Cache/Cache.cpp
+++ Cache/Cache.cpp
@@ -2,19 +2,69 @@
 //
 
 #include <iostream>
+#include <vector>
+#include <unordered_map>
+#include <list>
+#include <cstdio>
+using namespace std;
+
+// https://leetcode.com/problems/lru-cache/
+class LRUCache {
+    list<int> keyUsedLog;
+    unordered_map<int, pair<int, list<int>::iterator>> cache;  // key: (value, logIter)
+    int maxSize;
+    void touch(unordered_map<int, pair<int, list<int>::iterator>>::iterator cacheIter) {
+        int key = cacheIter->first;
+        auto logIter = cacheIter->second.second;
+        keyUsedLog.erase(logIter);
+        keyUsedLog.push_front(key);
+        cacheIter->second.second = keyUsedLog.begin();
+    }
+public:
+    LRUCache(int capacity) {
+        maxSize = capacity;
+    }
+    int get(int key) {
+        auto cacheIter = cache.find(key);
+        if (cacheIter == cache.end())
+            return -1;
+        touch(cacheIter);
+        return cacheIter->second.first;
+    }
+    void put(int key, int value) {
+        if (maxSize == 0) return;
+        auto cacheIter = cache.find(key);
+        if (cacheIter != cache.end()) {
+            touch(cacheIter);
+        }
+        else {
+            if (keyUsedLog.size() >= maxSize) {
+                cache.erase(keyUsedLog.back());
+                keyUsedLog.pop_back();
+            }
+            keyUsedLog.push_front(key);
+        }
+        cache[key] = { value, keyUsedLog.begin() };
+    }
+};
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    char action;
+    int capacity, key, value;
+    cin >> capacity;
+    LRUCache lru = LRUCache(capacity);
+    while (cin >> action) {
+        if (action == 'p') {
+            cin >> key >> value;
+            lru.put(key, value);
+        }
+        else if (action == 'g') {
+            cin >> key;
+            cout << lru.get(key) << endl;
+        }
+        else
+            break;
+    }
+    return 0;
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
