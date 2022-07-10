ba0d69328e354da718bd5f85b3b00ba0643083b8
xavierwu <whosxavierwu@gmail.com>
2022-07-10 15:17:52 +0800

    done 287

--- SearchInArray/leetcode_287.cpp
+++ SearchInArray/leetcode_287.cpp
@@ -16,24 +16,63 @@ using namespace std;
 
 class Solution {
 public:
-	int findDuplicate(vector<int>& nums) {
-		// v1: faster than 77.14% 
-		int p_step1 = nums[0];
-		int p_step2 = nums[0];
-		// find some point in the cycle
-		do {
-			p_step1 = nums[p_step1];
-			p_step2 = nums[nums[p_step2]];
-		} while (p_step1 != p_step2);
-		// find the entrance to the cycle 
-		// why does this work???
-		int entry = nums[0];
-		while (entry != p_step1) {
-			entry = nums[entry];
-			p_step1 = nums[p_step1];
-		}
-		return entry;
-	}
+    int findDuplicate1(vector<int>& nums) {  // 205ms
+        int p_step1 = nums[0];
+        int p_step2 = nums[0];
+        // find some point in the cycle
+        do {
+            p_step1 = nums[p_step1];
+            p_step2 = nums[nums[p_step2]];
+        } while (p_step1 != p_step2);
+        // find the entrance to the cycle 
+        // why does this work???
+        int entry = nums[0];
+        while (entry != p_step1) {
+            entry = nums[entry];
+            p_step1 = nums[p_step1];
+        }
+        return entry;
+    }
+    int findDuplicate2(vector<int>& nums) {  // 177ms
+        int n = nums.size() - 1;
+        int left = 1, right = n;
+        while (left < right) {
+            int a = (right - left + 1) % 2;
+            int mid = (left + right) / 2;
+            int lCnt = 0, rCnt = 0;
+            if (a == 1) {
+                int mCnt = 0;
+                for (int num: nums) {
+                    if (num < left || num > right)
+                        continue;
+                    lCnt += (num < mid);
+                    rCnt += (num > mid);
+                    mCnt += (num == mid);
+                    if (mCnt > 1) {
+                        return mid;
+                    }
+                }
+                if (lCnt > rCnt) {
+                    right = mid - 1;
+                } else {  // lCnt < rCnt
+                    left = mid + 1;
+                }
+            } else {
+                for (int num: nums) {
+                    if (num < left || num > right)
+                        continue;
+                    lCnt += (num <= mid);
+                    rCnt += (num > mid);
+                }
+                if (lCnt > rCnt) {
+                    right = mid;
+                } else {
+                    left = mid + 1;
+                }
+            }
+        }
+        return left;
+    }
 };
 int main()
 {
